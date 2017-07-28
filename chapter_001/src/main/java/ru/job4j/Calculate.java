27393278f0fd3399262744ba24db42add75b83f2
Bauyrzhan.Aigelov <baurzhana@gmail.com>
2017-07-28 12:17:26 +0600

    added javadoc comments
    Signed-off-by: Bauyrzhan.Aigelov <baurzhana@gmail.com>

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задачи части 001 урок1.
+ *author baigelov
+ *@since 28.07.2017
+*/
 public class Calculate{
-	
+	/**
+	* Главный метод main
+	* @param args массив строк
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+/**
+* Package for calculate task.
+*
+* @author Bauyrzhan Aigelov (mailto:baurzhan.aigelov@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
